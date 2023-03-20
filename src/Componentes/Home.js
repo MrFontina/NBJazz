2c066a0d09e4a459be7d0ca6382308903e78665f
Nahuel Bracchitta <nahuelbracchitta@gmail.com>
2023-03-19 23:05:55 -0300

    primeros pasos

---
+++ src/Componentes/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <main>
+        ¡Hola! Soy Nahuel Bracchitta, músico y compositor. Mi música se inspira en la fusión de diferentes estilos y culturas, creando una mezcla única de sonidos y emociones. En este sitio encontrarás información sobre mi música, videos, fotos y mucho más. ¡Bienvenido!
+    </main>
+  )
+}
+
+export default Home
