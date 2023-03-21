c9bb4f73c97c7847106f7ce89efe0208165928e6
Nahuel Bracchitta <nahuelbracchitta@gmail.com>
2023-03-21 17:40:51 -0300

    problemas con grid

--- src/Componentes/Guitarra.js
+++ src/Componentes/Guitarra.js
@@ -1,28 +1,43 @@
 import React from 'react'
 
-function Guitarra({isOpen}) {
+function Guitarra({ isOpen }) {
   return (
     <main className={`main-section  ${isOpen ? "" : "main-section-no-margin"}`}>
       <h3 className='subtitle'>CLASES DE GUITARRA</h3>
-      <div>
-        <p>
-        En mis clases de guitarra, me enfoco en diseñar un plan de estudio personalizado para cada estudiante, adaptado a sus intereses musicales, su disponibilidad horaria y sus objetivos. Este enfoque permite que las clases sean más efectivas y que los estudiantes puedan avanzar de manera más rápida y consistente.</p>
-
-<p> Entre los temas que podemos trabajar juntos, se incluyen: </p>
-<ul>
-  <li>Conocimiento integral del diapasón: aprenderás a visualizar el diapasón completo de la guitarra y a entender cómo se relacionan las diferentes notas y acordes.</li>
-  <li>Armonía y construcción de acordes: exploraremos diferentes tipos de acordes y su función dentro de la música. También aprenderás a construir tus propios acordes y a utilizarlos en tus composiciones e improvisaciones.</li>
-  <li>Escalas: abarcando desde las escalas básicas hasta las más avanzadas, incluyendo la mayor, menor melódica, menor armónica, mayor armónica, disminuida, tonal, cromática y pentatónicas, con sus respectivos arpegios. También te enseñaré formas creativas de pensar arpegios o patterns nuevos.</li>
-  <li>Intervalos: entenderás cómo se relacionan las diferentes notas entre sí y cómo puedes utilizar esta información para crear solos más interesantes y melodías más complejas.</li>
-  <li>Sonido: trabajaremos en mejorar la calidad de tu sonido y en encontrar tu propia voz como guitarrista.</li>
-  <li>Técnicas de púa: exploraremos diferentes técnicas de púa y cómo aplicarlas en diferentes contextos musicales.</li>
-</ul>
-<p>
-  Además, las clases de guitarra también incluyen todo el contenido de las clases de improvisación, para que puedas aplicar todo lo que aprendas en contextos musicales reales y desarrollar tus habilidades como improvisador.
-</p>
-<p>
-  No dudes en contactarme para más información o para programar una clase de prueba. ¡Estoy ansioso por ayudarte a alcanzar tus metas como guitarrista y músico de jazz!
-</p>       
+      <div className='container-guitarra'>
+        <img src='../../Images/guitarracontrapedales.jpg' className='guitarra-pic'   alt='guitarra y contrabajo' id='guitarra-title'></img>
+        <p id='guit-p-1'>
+          En mis clases de guitarra, me enfoco en diseñar un plan de estudio personalizado para cada estudiante, adaptado a sus intereses musicales, su disponibilidad horaria y sus objetivos. Este enfoque permite que las clases sean más efectivas y que los estudiantes puedan avanzar de manera más rápida y consistente.</p>
+        <img src='../../Images/guitarra.jpg' className='guitarra-pic' alt='guitarra y pedales' id='guitarra-vertical'></img>
+    <div id='guit-grid-medio'>
+      <p > Entre los temas que podemos trabajar juntos, se incluyen: </p>
+        <ul >
+          <li><p className='p-title'>Conocimiento integral del diapasón: </p>
+            <p>Aprenderás a visualizar el diapasón completo de la guitarra y a entender cómo se relacionan las diferentes notas y acordes.
+            </p></li>
+          <li><p className='p-title'> Armonía y construcción de acordes: </p>
+            <p>Exploraremos diferentes tipos de acordes y su función dentro de la música. También aprenderás a construir tus propios acordes y a utilizarlos en tus composiciones e improvisaciones.
+            </p></li>
+          <li><p className='p-title'>Escalas: </p>
+            <p>Abarcando desde las escalas básicas hasta las más avanzadas, incluyendo la mayor, menor melódica, menor armónica, mayor armónica, disminuida, tonal, cromática y pentatónicas, con sus respectivos arpegios. También te enseñaré formas creativas de pensar arpegios o patterns nuevos. </p> </li>
+          <li><p className='p-title'>Intervalos:  </p>
+            <p>Entenderás cómo se relacionan las diferentes notas entre sí y cómo puedes utilizar esta información para crear solos más interesantes y melodías más complejas.
+            </p></li>
+          <li><p className='p-title'>Sonido: </p>
+            <p>Trabajaremos en mejorar la calidad de tu sonido y en encontrar tu propia voz como guitarrista.
+            </p></li>
+          <li>
+            <p className='p-title'>Técnicas de púa:
+            </p>
+            <p>Exploraremos diferentes técnicas de púa y cómo aplicarlas en diferentes contextos musicales.
+            </p></li>
+        </ul>
+    </div>
+        
+        <img src='../../Images/riendo.jpg' className='guitarra-pic' alt='nahuel bracchitta y hernan cassibba' id='riendo'></img>
+        <p id='guit-p-3'>
+          Además, las clases de guitarra también incluyen todo el contenido de las clases de improvisación, para que puedas aplicar todo lo que aprendas en contextos musicales reales y desarrollar tus habilidades como improvisador.
+        </p>
       </div>
     </main>
   )
