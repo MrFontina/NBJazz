4b87397c1b819ab7d781a47d24de7782c70c7ef9
Nahuel Bracchitta <nahuelbracchitta@gmail.com>
2023-03-21 16:08:07 -0300

    cambios

--- src/Componentes/About.js
+++ src/Componentes/About.js
@@ -12,10 +12,10 @@ function About({ isOpen }) {
         {/* <img src='../../Images/2.JPG' alt='mi'id="img1"></img> */}
         <p id='p1'>
           Guitarrista de jazz de Buenos Aires. Cuenta con un disco solista y mas de 6 discos grabados en colaboración. Egresado de la Tecnicatura superior en jazz del Conservatorio Superior de Musica Manuel de Falla, luego de haber estudiado en la Escuela de Arte Leopoldo Marechal.</p>
-        <img src='../../Images/12.png' alt="grabando en estudio insigno" id="img2"></img>
+        <img src='../../Images/12.png' alt="grabando en estudio insigno" id="img2" loading="lazy"></img>
         <p id='p2'>Como guitarrista e improvisador, se formó manera particular con Martin Porto, Ricardo Cavalli y Juan Cruz de Urquiza entre otros. También participó de ensambles con Mariano Loiacono, Sergio Verdinelli, Mariano Otero, Tony Malaby y David Kikoski.
         </p>
-        <img src='../../Images/3.JPG' alt="guitar" id="img3"></img>
+        <img src='../../Images/3.JPG' alt="guitar" id="img3" loading="lazy"></img>
 
         <p id='p3'>En el 2014 realiza un viaje a Nueva York donde se perfecciona con Adam Rogers, Ben Monder, Mike Moreno, Steve Cardenas y Nir Felder. En el 2017 realiza un workshop de dos meses con el maestro Wayne Krantz Asistió a clínicas con Ari Hoenig, Jonathan Kreisberg, Miguel Zenon, Peter Bernstein y Mike Moreno.
         </p>
