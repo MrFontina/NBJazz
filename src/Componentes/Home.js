5d87a5a8fdf615ef3c3f5cee71ba83fd78ceecfd
Nahuel Bracchitta <nahuelbracchitta@gmail.com>
2023-03-22 11:15:33 -0300

    menor peso en img y correcciones de gramatica

--- src/Componentes/Home.js
+++ src/Componentes/Home.js
@@ -11,7 +11,7 @@ function Home({ isOpen }) {
         <p>
          ¡Hola! Soy Nahuel Bracchitta, guitarrista de <span>jazz</span>. Participo activamente de la vibrante escena del jazz de la ciudad de <span>Buenos Aires</span>, tanto con mi proyecto personal asi como colaborando con algunos de mis músicos favoritos!<br/>
          
-          A lo largo de los años, he adquirido una gran cantidad de conocimientos sobre el arte de tocar la guitarra y la teoría musical. Como resultado, he decidido compartir todo lo que he he aprendido a través de <span>clases</span>, tanto presenciales como virtuales.       
+          A lo largo de los años, he adquirido una gran cantidad de conocimientos sobre el arte de tocar la guitarra y la teoría musical. Desde hace 15 años me dedico a dar <span>clases</span> de <span> guitarra</span> e <span>improvisación</span>, tanto <span>presenciales</span> como <span>virtuales para todo el mundo</span>.       
          <br/>
            Mi objetivo es ayudar a otros músicos a desarrollar sus habilidades y alcanzar sus objetivos artísticos, sin importar su nivel de experiencia.
          </p>
